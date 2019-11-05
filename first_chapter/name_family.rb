a455b599f3b15117d11bf6f1a38a87376e81e0d9
Dnpypy <denlatypoff@yandex.ru>
2019-11-06 00:50:30 +0300

    first commit, first programm, first chapter

---
+++ first_chapter/name_family.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+=begin
+Моя первое задание по самоучителю Ruby.
+=end
+
+puts "Whats your name?"
+name    = gets.strip.capitalize
+
+puts "Whats your name?"
+surname = gets.strip.capitalize
+
+puts "My full name #{name} #{surname}"
